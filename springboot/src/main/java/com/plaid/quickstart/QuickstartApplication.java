792abf928a3088443e95f712793a3292efaf1e38
admin <srivatsam6@gmail.com>
2019-11-13 22:40:49 -0800

    Plaid Integration springboot Initial

--- springboot/src/main/java/com/plaid/quickstart/QuickstartApplication.java
+++ springboot/src/main/java/com/plaid/quickstart/QuickstartApplication.java
@@ -1,15 +1,27 @@
 package com.plaid.quickstart;
 
+import com.plaid.client.PlaidClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class QuickstartApplication {
-
+	private static PlaidClient plaidClient;
 	public static void main(String[] args) {
-		SpringApplication.run(QuickstartApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(QuickstartApplication.class, args);
+
+		String plaidClientId = ctx.getEnvironment().getProperty("plaidClientID");
+		String plaidSecret = ctx.getEnvironment().getProperty("plaidSecret");
+		String plaidPublicKey = ctx.getEnvironment().getProperty("plaidPublicKey");
+
+		plaidClient = PlaidClient.newBuilder()
+				.clientIdAndSecret(plaidClientId, plaidSecret)
+				.publicKey(plaidPublicKey) // optional. only needed to call endpoints that require a public key
+				.developmentBaseUrl() // or equivalent, depending on which environment you're calling into
+				.build();
 	}
 
 }
