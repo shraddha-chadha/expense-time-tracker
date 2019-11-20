bd215816ca28383eabbc74a080bcb6e1b549b364
shraddha-chadha <shradhachadha08@gmail.com>
2019-11-20 09:39:44 +0000

    Missing files

---
+++ voice-assistant-server/google_home/intents/highest-expense.js
@@ -0,0 +1,5 @@
+module.exports = () => {
+    return {
+        "fulfillmentText": "Your highest expense was $1023.12 in Mariott LA"
+    }
+};
---
+++ voice-assistant-server/google_home/intents/last-transaction.js
@@ -0,0 +1,5 @@
+module.exports = () => {
+    return {
+        "fulfillmentText": "Your last transaction was $5"
+    }
+};
