e30d5414546fada9505aadb23a64d6ef8508bbbb
admin <srivatsam6@gmail.com>
2019-11-21 17:52:25 -0800

    Get All totals

--- springboot/src/main/java/com/plaid/quickstart/repository/TransactionRepository.java
+++ springboot/src/main/java/com/plaid/quickstart/repository/TransactionRepository.java
@@ -4,8 +4,15 @@ import com.plaid.quickstart.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 
+    List<Transaction> findByMonthAndYearAndUser_id(Integer month, Integer year, Integer user_id);
+
+    List<Transaction> findByQuarterAndYearAndUser_id(Integer quarter, Integer year, Integer user_id);
+
+    List<Transaction> findByYearAndUser_id(Integer year, Integer user_id);
 
 }
--- springboot/src/main/java/com/plaid/quickstart/service/TransactionService.java
+++ springboot/src/main/java/com/plaid/quickstart/service/TransactionService.java
@@ -15,7 +15,7 @@ import retrofit2.Response;
 
 import javax.persistence.RollbackException;
 import java.io.IOException;
-import java.util.Date;
+import java.util.*;
 
 @Service
 public class TransactionService {
@@ -160,5 +160,87 @@ public class TransactionService {
             quarter = 4;
 
         return quarter;
+    }
+
+
+
+    public Map<String,Double> getAllTotalsByFilter(Integer userId, String filterType, Integer month, Integer quarter, Integer year) throws RollbackException, ResourceNotFoundException{
+        Map<String,Double> map = new HashMap<>();
+        List<Transaction> transactions = null;
+        switch (filterType){
+            case "M":
+                transactions = transactionRepository.findByMonthAndYearAndUser_id(month,year,userId);
+                break;
+
+            case "Q":
+                transactions = transactionRepository.findByQuarterAndYearAndUser_id(quarter,year,userId);
+                break;
+
+            case "Y":
+                transactions = transactionRepository.findByYearAndUser_id(year,userId);
+                break;
+
+
+        }
+
+        if(transactions!=null){
+            map = getAllTotalsByFilter(transactions);
+        }
+        else
+        {
+            throw new ResourceNotFoundException("Transactions","Year/Month/Quarter","Not found");
+        }
+
+        return map;
+    }
+
+    //totalExpense, totalIncome, totalBudget, totalSavings
+    public Map<String,Double> getAllTotalsByFilter(List<Transaction> transactions)
+    {
+        String[] arr = new String[]{"Income","Budget"};
+        List<String> list = Arrays.asList(arr);
+
+        Map<String,Double> map = new HashMap<>();
+        for(Transaction transaction: transactions)
+        {
+        if(list.contains(transaction.getTransactionCategory()))
+        {
+            if(transaction.getTransactionCategory().equalsIgnoreCase("Income"))
+            {
+                if(map.containsKey("totalIncome")){
+                    map.put("totalIncome", map.get("totalIncome")+transaction.getAmount());
+                }
+                else
+                {
+                    map.put("totalIncome",transaction.getAmount());
+                }
+            }
+            else
+            {
+                if(map.containsKey("totalBudget")){
+                    map.put("totalBudget", map.get("totalBudget")+transaction.getAmount());
+                }
+                else
+                {
+                    map.put("totalBudget",transaction.getAmount());
+                }
+            }
+        }
+        else
+        {
+            if(map.containsKey("totalExpense")){
+                map.put("totalExpense", map.get("totalExpense")+transaction.getAmount());
+            }
+            else
+            {
+                map.put("totalExpense",transaction.getAmount());
+            }
+        }
+    }
+        if(map.containsKey("totalIncome") && map.containsKey("totalExpense"))
+        {
+            map.put("totalSavings", map.get("totalIncome")-map.get("totalExpense"));
+        }
+        return map;
     }
 }
