0541c514c8638c79a466833be66a469e83a9299c
Shraddha Chadha <shradhachadha08@gmail.com>
2020-02-02 20:51:03 -0800

    Updated plaid apis

--- client/src/env.js
+++ client/src/env.js
@@ -2,8 +2,8 @@
 module.exports = {
     backendUrl: "http://localhost:8080",           // SERVER BACKEND URL
     frontendUrl: "http://localhost:3000",
-    PLAID_CLIENT_ID: '5d34c578dc87a60014247747',
-    PLAID_PUBLIC_KEY: '408076128548bef334534e21b01171',
-    PLAID_SECRET: '625b4630199b5418cb7eb17e41746b',
+    PLAID_CLIENT_ID: '5e2d04eea5e7f0001225444d',
+    PLAID_PUBLIC_KEY: '51eee13236b0ea2359994ac5ccb7e8',
+    PLAID_SECRET: 'dad7ae0d608257786481b65f4bec08',
     PLAID_ENV:'development'
 };
